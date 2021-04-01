83cba5044df28d733bd9ab92de6a09fd79f8d928
BradenTerk <bradenterk@gmail.com>
2021-04-01 00:31:23 +0000

    database update

--- config/database.rb
+++ config/database.rb
@@ -4,13 +4,26 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
+  if Sinatra::Application.development?
+    set :database, {
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
+  else
+    db_url = postgres://tcxstshtydades:f0757598f1144b6377d0d3389d869361e77a3170f2f1503e7e91ca506892f529@ec2-54-159-175-113.compute-1.amazonaws.com:5432/dds7qklcvuke32
+    db = URI.parse(ENV['DATABASE_URL'] || db_url)
+    set :database, {
+      adapter: "postgresql",
+      host: db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
 
   # Load all models from app/models, using autoload instead of require
-    postgres://tcxstshtydades:f0757598f1144b6377d0d3389d869361e77a3170f2f1503e7e91ca506892f529@ec2-54-159-175-113.compute-1.amazonaws.com:5432/dds7qklcvuke32
+  # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
